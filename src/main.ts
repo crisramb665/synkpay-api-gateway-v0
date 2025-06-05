98f78b80b05897809228c67b7b4390ee8af30d5f
Cristian Romero <cristianrom66@gmail.com>
2025-06-05 16:25:24 -0500

    feat(docs): swagger config added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 /** npm imports */
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 /** local imports */
 import { AppModule } from './app.module'
@@ -10,11 +13,26 @@ async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule)
 
     const configService = app.get(ConfigService)
-
+    const swaggerPath = configService.get<string>('SWAGGER_PATH') || 'api/docs'
     const port = configService.get<number>('PORT') || 5000
 
+    const packageJsonReference = JSON.parse(readFileSync(join(__dirname, '..', 'package.json'), 'utf-8')) as {
+      version: string
+    }
+
+    //TODO: Add bearerAuth method once JWT authentication is implemented
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Synk Pay API Gateway')
+      .setDescription('API Gateway to interact with Synk Pay microservices')
+      .setVersion(packageJsonReference.version)
+      .build()
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup(swaggerPath, app, document)
+    console.log(`Swagger documentation available at: http://localhost:${port}/${swaggerPath}`)
+
     await app.listen(port)
-    console.log(`Application is running on: http://localhost:${port}`)
+    console.log(`🚀🚀🚀 Application is running on: http://localhost:${port}`)
   } catch (error: unknown) {
     if (error instanceof Error) console.error('Error during bootstrap:', error.message)
     else console.error('Error during bootstrap:', error)
