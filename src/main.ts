2d0d3538bfad0c16d560fb60bf2dacee75b342df
Cristian Romero <cristianrom66@gmail.com>
2025-06-05 15:59:45 -0500

    feat: - using ConfigModule on app entrypoint - new env added to env.example and config file - swagger package for nest installed

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 /** npm imports */
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
 
 /** local imports */
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
-}
-bootstrap()
-  .then(() => {
-    console.log(`Server is running on port ${process.env.PORT ?? 3000}`)
-  })
-  .catch((error) => {
-    console.error('Error starting the server:', error)
+async function bootstrap(): Promise<void> {
+  try {
+    const app = await NestFactory.create(AppModule)
+
+    const configService = app.get(ConfigService)
+    const port = configService.get<number>('PORT') || 5000
+
+    await app.listen(port)
+    console.log(`Application is running on: http://localhost:${port}`)
+  } catch (error: unknown) {
+    if (error instanceof Error) console.error('Error during bootstrap:', error.message)
+    else console.error('Error during bootstrap:', error)
+
     process.exit(1)
-  })
+  }
+}
+
+void bootstrap()
