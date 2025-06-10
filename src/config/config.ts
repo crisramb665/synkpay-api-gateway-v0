4c275169d4a213781c88b11476c5d0a2bc7e772e
Cristian Romero <cristianrom66@gmail.com>
2025-06-10 11:34:16 -0500

    fix(config): port 5000 restored

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  PORT: parseInt(process.env.PORT ?? '5001', 10), // 5000 already in use
+  PORT: parseInt(process.env.PORT ?? '5000', 10), // 5000 already in use
   NODE_ENV: process.env.NODE_ENV ?? 'development',
   SWAGGER_PATH: process.env.SWAGGER_PATH ?? 'api/docs',
   API_KEY: process.env.API_KEY,
