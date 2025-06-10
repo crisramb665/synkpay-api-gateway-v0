a3e47b50f00cdb572677b6d82848e87684dda75f
victoriabonanni <victoria.ayui@gmail.com>
2025-06-10 12:10:54 -0300

    feat: throttler guard implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 /** npm imports */
 import { join } from 'path'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
 
 /** local imports */
 import { HealthModule } from '../health/health.module'
 import config from '../config/config'
+import { CustomRateLimiterGuard } from '../rate-limit/rate-limit-custom.guard'
 
 const SCHEMA_PATH = join(process.cwd(), 'src/graphql/schema.gql')
 
@@ -26,9 +29,21 @@ const SCHEMA_PATH = join(process.cwd(), 'src/graphql/schema.gql')
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ([{
+        ttl: (configService.get<number>('RATE_LIMIT_WINDOW_SEC') ?? 60), 
+        limit: configService.get<number>('RATE_LIMIT_GLOBAL') ?? 10,
+      }]),
+    }),
     HealthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: CustomRateLimiterGuard,
+    },
+  ],
 })
 export class AppModule {}
